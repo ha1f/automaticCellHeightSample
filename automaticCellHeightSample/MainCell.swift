4d8d3d22225c62a7da238f02f7371cf8f4d2f53c
ha1f <ha1f@linecorp.com>
2018-07-19 11:40:48 +0900

    implemented

---
+++ automaticCellHeightSample/MainCell.swift
@@ -0,0 +1,13 @@
+//
+//  MainCell.swift
+//  automaticCellHeightSample
+//
+//  Created by ST20591 on 2018/07/19.
+//  Copyright © 2018年 ha1f. All rights reserved.
+//
+
+import UIKit
+
+class MainCell: UITableViewCell {
+    @IBOutlet var label: UILabel!
+}
--- automaticCellHeightSample/ViewController.swift
+++ automaticCellHeightSample/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var items: [String] = [
+        "aiueo",
+        "The quick brown fox jumps over the lazy dog",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        ] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "mainCell") as? MainCell else {
+            return UITableViewCell()
+        }
+        cell.label.text = items[indexPath.row]
+        return cell
+    }
 }
 
