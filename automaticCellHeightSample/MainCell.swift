c8c913318c61d1df4c17dd988f954daba70f995f
ha1f <ha1f@linecorp.com>
2018-07-20 18:03:52 +0900

    fixd trailing margin

--- automaticCellHeightSample/MainCell.swift
+++ automaticCellHeightSample/MainCell.swift
@@ -29,7 +29,7 @@ class MainCell: UITableViewCell {
         NSLayoutConstraint.activate([
             label.topAnchor.constraint(equalTo: topAnchor, constant: 10),
             label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 5),
-            label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 5),
+            label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -5),
             label.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10),
             ])
     }
